646cb7c5b6b5adcf8e08ed1ececb55c63e6cb004
Jhnanesh07 <jhnaneshshetty106@gmail.com>
2026-06-16 00:23:36 +0530

    Added JD based ATS matching

--- app.py
+++ app.py
@@ -3,14 +3,18 @@ from skills import SKILLS
 
 app = Flask(__name__)
 
+
 @app.route('/')
 def home():
     return render_template('index.html')
+
 
 @app.route('/analyze', methods=['POST'])
 def analyze():
 
     file = request.files['resume']
+
+    job_description = request.form['job_description']
 
     import PyPDF2
 
@@ -21,6 +25,7 @@ def analyze():
     for page in pdf_reader.pages:
         text += page.extract_text()
 
+
     found_skills = []
 
     for skill in SKILLS:
@@ -29,7 +34,47 @@ def analyze():
 
             found_skills.append(skill)
 
-    score = (len(found_skills) / len(SKILLS)) * 100
+
+    jd_skills = []
+
+    for skill in SKILLS:
+
+        if skill.lower() in job_description.lower():
+
+            jd_skills.append(skill)
+
+
+    matched_skills = []
+
+    for skill in jd_skills:
+
+        if skill in found_skills:
+
+            matched_skills.append(skill)
+
+        missing_jd_skills = []
+
+    for skill in jd_skills:
+
+        if skill not in found_skills:
+
+            missing_jd_skills.append(skill)    
+
+    if len(jd_skills) > 0:
+
+        match_score = (
+            len(matched_skills)
+            / len(jd_skills)
+        ) * 100
+
+    else:
+
+        match_score = 0
+
+    score = (
+        len(found_skills)
+        / len(SKILLS)
+    ) * 100
 
     missing_skills = []
 
@@ -38,30 +83,44 @@ def analyze():
         if skill not in found_skills:
 
             missing_skills.append(skill)
+
+    # Suggestions
+
     suggestions = []
 
-    if "aws" in missing_skills:
-        suggestions.append("Learn AWS Cloud Fundamentals")
+    if "aws" in missing_jd_skills:
+        suggestions.append(
+            "Learn AWS Cloud Fundamentals"
+        )
 
-    if "git" in missing_skills:
-        suggestions.append("Create and maintain a GitHub portfolio")
+    if "git" in missing_jd_skills:
+        suggestions.append(
+            "Create and maintain a GitHub portfolio"
+        )
 
-    if "react" in missing_skills:
-        suggestions.append("Build a React project")
+    if "react" in missing_jd_skills:
+        suggestions.append(
+            "Build a React project"
+      )
 
-    if "sql" in missing_skills:
-        suggestions.append("Practice SQL queries and database concepts")
+    if "sql" in missing_jd_skills:
+        suggestions.append(
+            "Practice SQL queries and database concepts"
+    )
 
-    if "python" in missing_skills:
-        suggestions.append("Strengthen Python programming skills")        
+    if "python" in missing_jd_skills:
+        suggestions.append(
+            "Strengthen Python programming skills"
+    )
 
     return render_template(
-    'result.html',
-    score=round(score),
-    found_skills=found_skills,
-    missing_skills=missing_skills,
-    suggestions=suggestions
-)    
+        'result.html',
+        match_score=round(match_score),
+        matched_skills=matched_skills,
+        missing_jd_skills=missing_jd_skills,
+        suggestions=suggestions
+    )
+
 
 if __name__ == '__main__':
     app.run(debug=True)
